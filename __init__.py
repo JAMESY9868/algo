7d0ad4b548a97bc12df0802a94a05ef47e755209
Ruijie Yu <ruijie.yu@outlook.com>
2019-06-01 16:42:48 +0800

    added stub main and simple init

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env -S python3 #-i
+'''
+    This package supplies with some algorithms to be reused in
+    other projects.
+'''
 
+from . import shared;
+from . import sort;
--- __main__.py
+++ __main__.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env -S python3 #-i
 
+from . import sort, shared;
+
+print(dir(shared));
+print(dir(sort));
