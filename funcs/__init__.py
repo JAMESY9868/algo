3e0b28c573c25ab4045768fdc25a5e5aced7a401
Ruijie Yu <ruijie.yu@outlook.com>
2019-06-02 12:17:02 +0800

    added init&main for funcs

---
+++ funcs/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S python3 -i
+'''
+    Provides some higher-order functions or useful utilities
+'''
+
+import typing;
+
+from . import shared;
+from . import keyDispatch as kd;
+from . import typeDispatch as td;
+
+__all__: typing.Tuple[str, ...] = (
+    ## builtin packages
+    'typing', #'c', 'fts',
+    ## internal modules intended for exposure
+    'shared', 'kd', 'td',
+    ## external modules
+    #'algo',
+    ## misc
+    #'Function', 'Decorator',
+);
---
+++ funcs/__main__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S python3 -i
+
+from . import *;
+
+print(globals());
