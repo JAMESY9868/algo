fbabe3f07f547cb4d5d5052e81b4a80eaf1c2e2b
Ruijie Yu <ruijie.yu@outlook.com>
2019-06-10 22:57:46 +0800

    minor fixes on algo.funcs.shared

--- funcs/shared.py
+++ funcs/shared.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env -S python3 -i
+# pylint: disable=invalid-name
 
 import typing;
-import functools as fts;
-import collections as c;
+
+from ..shared import _slots;
+# import functools as fts;
+# import collections as c;
 
 # types and quick accesses
 T = typing.TypeVar('T');
-class Function(typing.Callable[..., T]): pass;
+class Function(typing.Callable[..., T]):
+    pass;
 Decorator: object = typing.Callable[[Function], Function];
+
+__all__: _slots = (
+    'Function',
+    'Decorator',
+);
